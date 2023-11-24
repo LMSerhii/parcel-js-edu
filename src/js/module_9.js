175f2729ec41fcf89c8cc603d0fcb94ed9fa62a7
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-11-24 20:11:42 +0200

    Update module_9.js

--- src/js/module_9.js
+++ src/js/module_9.js
@@ -1,79 +1,79 @@
 // import * as basicLightbox from 'basiclightbox';
 
-const box = document.querySelector('.js-box');
-const timeText = document.querySelector('.js-time');
-const timeText2 = timeText.nextElementSibling;
-const timeText3 = timeText2.nextElementSibling;
-const timeText4 = timeText3.nextElementSibling;
-const timeText5 = timeText4.nextElementSibling;
-const timeText6 = timeText5.nextElementSibling;
-const timeText7 = timeText6.nextElementSibling;
+// const box = document.querySelector('.js-box');
+// const timeText = document.querySelector('.js-time');
+// const timeText2 = timeText.nextElementSibling;
+// const timeText3 = timeText2.nextElementSibling;
+// const timeText4 = timeText3.nextElementSibling;
+// const timeText5 = timeText4.nextElementSibling;
+// const timeText6 = timeText5.nextElementSibling;
+// const timeText7 = timeText6.nextElementSibling;
 
-const days = [
-  'Неділя',
-  'Понеділок',
-  'Вівторок',
-  'Середа',
-  'Четвер',
-  "П'ятниця",
-  'Суббота',
-];
+// const days = [
+//   'Неділя',
+//   'Понеділок',
+//   'Вівторок',
+//   'Середа',
+//   'Четвер',
+//   "П'ятниця",
+//   'Суббота',
+// ];
 
-const months = [
-  'Січень',
-  'Лютий',
-  'Березень',
-  'Квітень',
-  'Травень',
-  'Червень',
-  'Липень',
-  'Серпень',
-  'Вересень',
-  'Жовтень',
-  'Листопад',
-  'Грудень',
-];
+// const months = [
+//   'Січень',
+//   'Лютий',
+//   'Березень',
+//   'Квітень',
+//   'Травень',
+//   'Червень',
+//   'Липень',
+//   'Серпень',
+//   'Вересень',
+//   'Жовтень',
+//   'Листопад',
+//   'Грудень',
+// ];
 
-setInterval(() => {
-  const date = new Date();
-  timeText.textContent = date;
-  timeText2.textContent = 'День місяця: ' + date.getDate();
-  timeText3.textContent = 'День тижня: ' + days[date.getDay()];
-  timeText4.textContent = 'Рік: ' + date.getFullYear();
-  timeText5.textContent = 'Місяць: ' + months[date.getMonth()];
-  timeText6.textContent = `Час: ${date
-    .getHours()
-    .toString()
-    .padStart(2, '0')} : ${date
-    .getMinutes()
-    .toString()
-    .padStart(2, '0')} : ${date.getSeconds().toString().padStart(2, '0')} `;
-  timeText7.textContent = Date.now();
-}, 1000);
+// setInterval(() => {
+//   const date = new Date();
+//   timeText.textContent = date;
+//   timeText2.textContent = 'День місяця: ' + date.getDate();
+//   timeText3.textContent = 'День тижня: ' + days[date.getDay()];
+//   timeText4.textContent = 'Рік: ' + date.getFullYear();
+//   timeText5.textContent = 'Місяць: ' + months[date.getMonth()];
+//   timeText6.textContent = `Час: ${date
+//     .getHours()
+//     .toString()
+//     .padStart(2, '0')} : ${date
+//     .getMinutes()
+//     .toString()
+//     .padStart(2, '0')} : ${date.getSeconds().toString().padStart(2, '0')} `;
+//   timeText7.textContent = Date.now();
+// }, 1000);
 
-const timer = document.querySelector('.js-timerBox');
-const timer__counter = document.querySelector('.js-counter');
+// const timer = document.querySelector('.js-timerBox');
+// const timer__counter = document.querySelector('.js-counter');
 
-// console.log(timer__counter);
+// // console.log(timer__counter);
 
-let counter = 10;
+// let counter = 10;
 
-setTimeout(() => {
-  timer.style.display = 'block';
-  const intervalId = setInterval(() => {
-    timer__counter.textContent = counter.toString().padStart(2, '0');
-    if (!counter) {
-      timer__counter.textContent = 'x';
-      clearInterval(intervalId);
-      timer__counter.addEventListener('click', onClick);
-    }
-    counter -= 1;
-  }, 1000);
-}, 5000);
+// setTimeout(() => {
+//   timer.style.display = 'block';
+//   const intervalId = setInterval(() => {
+//     timer__counter.textContent = counter.toString().padStart(2, '0');
+//     if (!counter) {
+//       timer__counter.textContent = 'x';
+//       clearInterval(intervalId);
+//       timer__counter.addEventListener('click', onClick);
+//     }
+//     counter -= 1;
+//   }, 1000);
+// }, 5000);
 
-function onClick() {
-  timer.style.display = 'none';
-}
+// function onClick() {
+//   timer.style.display = 'none';
+// }
 
 // const timer = document.querySelector('.js-timerBox');
 // const timer__counter = document.querySelector('.js-counter');
@@ -115,23 +115,97 @@ function onClick() {
 
 // NOTIFICATIONS
 
-const refs = {
-  notification: document.querySelector('.js-note'),
+// const refs = {
+//   notification: document.querySelector('.js-note'),
+// };
+
+// refs.notification.addEventListener('click', onClick);
+
+// let timeoutId = null;
+
+// timeoutId = setTimeout(() => {
+//   closeNotification(refs.notification);
+// }, 10000);
+
+// function onClick(evt) {
+//   closeNotification(evt.target);
+//   clearTimeout(timeoutId);
+// }
+
+// function closeNotification(element) {
+//   element.style.translate = '320px 0';
+// }
+
+/**
+  |============================
+  | Promise
+  |============================
+*/
+
+// console.log('Request data ...');
+
+// setTimeout(() => {
+//   console.log('Preparing data ...');
+
+//   const data = {
+//     server: 'AWS',
+//     port: 2000,
+//     status: 'Active',
+//   };
+
+//   setTimeout(() => {
+//     data.modified = true;
+//     console.log('Received data', data);
+//   }, 3000);
+// }, 3000);
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log('Prepering data ...');
+
+//     const data = {
+//       server: 'AWS',
+//       port: 5000,
+//       status: 'Active',
+//     };
+
+//     resolve(data);
+//   }, 3000);
+// });
+
+// promise
+//   .then(data => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         data.modified = true;
+//         resolve(data);
+//       }, 3000);
+//     });
+//   })
+//   .then(clientData => {
+//     clientData.fromPromise = true;
+//     return clientData;
+//   })
+//   .then(data => {
+//     console.log('Modified', data);
+//   })
+//   .catch(err => {
+//     console.error('Error', err);
+//   })
+//   .finally(() => {
+//     console.log('Finally');
+//   });
+
+const sleep = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), ms);
+  });
 };
 
-refs.notification.addEventListener('click', onClick);
+// sleep(2000).then(() => console.log('After 2 seconds'));
+// sleep(4000).then(() => console.log('After 4 seconds'));
+// sleep(6000).then(() => console.log('After 6 seconds'));
 
-let timeoutId = null;
-
-timeoutId = setTimeout(() => {
-  closeNotification(refs.notification);
-}, 10000);
-
-function onClick(evt) {
-  closeNotification(evt.target);
-  clearTimeout(timeoutId);
-}
-
-function closeNotification(element) {
-  element.style.translate = '320px 0';
-}
+Promise.all([sleep(2000), sleep(4000), sleep(6000)]).then(() => {
+  console.log('Completed all promises');
+});
